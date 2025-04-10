f833ed4b5bd88ddcf3cd889b19bf23bc0310c212
saivenkataramireddy <141711350+saivenkataramireddy@users.noreply.github.com>
2025-04-10 16:53:50 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,23 @@
+#printing vowels and consonents in a word
+'''n=input("Enter a string")
+a="aeiouAEIOU"
+vowels=""
+cons= ""
+for i in n:
+    if i in a:
+        vowels +=i
+    else:
+        cons +=i
+print("Vowels",vowels)
+print("Consonents",cons)'''
+n=input("Enter a string")
+a="aeiouAEIOU"
+vowels=0
+cons=0
+for i in n:
+    if i in a:
+        vowels +=1
+    else:
+        cons +=1
+print("vowels",vowels)
+print("Consonents",cons)
