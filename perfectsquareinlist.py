0c5b33c453748473bf27f9d18fbec5ae174d13bc
saivenkataramireddy <141711350+saivenkataramireddy@users.noreply.github.com>
2025-04-30 22:23:01 +0530

    Add files via upload

--- perfectsquareinlist.py
+++ perfectsquareinlist.py
@@ -1,8 +1,10 @@
+#1) WAP to print perfect square numbers in the list ? 
 n=list(map(int,input("Enter a list").split()))
 for i in n:
     if (i**0.5)==int(i**0.5):
-        print("Perfect square")
-        
-    else:
-        print("Not perfect square")
-    
+        print(i)
+#2) WAP to print all the decimals numbers in the given input ?
+n=input("Enter").split()
+for i in n:
+    if "." in i:
+        print(i)
