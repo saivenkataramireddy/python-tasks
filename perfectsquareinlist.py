82b1685f53e4191af7dd5ae2439de6aaced54aa7
saivenkataramireddy <141711350+saivenkataramireddy@users.noreply.github.com>
2025-04-30 17:58:16 +0530

    Add files via upload

---
+++ perfectsquareinlist.py
@@ -0,0 +1,8 @@
+n=list(map(int,input("Enter a list").split()))
+for i in n:
+    if (i**0.5)==int(i**0.5):
+        print("Perfect square")
+        
+    else:
+        print("Not perfect square")
+    
--- stringcapital.py
+++ stringcapital.py
@@ -1,8 +1,12 @@
-#n=input("Enter a sting").split()
-n="take u forward is awesome".split()
-for i in n :
-    a=len(i)
-    b=""
-    for j in range(n):
-        if i==0 and i==a-1:
-            
+n=input("Enter")
+a="" 
+for i in n:
+    if i==" ":
+        if len(a)==1:
+            print(a.upper(),end=" ")
+        else:
+            print(a[0].upper()+a[1:len(a)-1]+a[-1].upper(),end=" ")        
+        a=""
+    else:
+        a = a+i  
+print(a[0].upper()+a[1:len(a)-1]+a[-1].upper(),end=" ")
